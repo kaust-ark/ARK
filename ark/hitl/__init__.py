9ffcb3ca503d5ba9e1c68dc2d073e35346624884
lyzx2001 <rep_laureline@hotmail.com>
2026-04-24 20:28:49 +0000

    Revert "fix(pipeline): restore HITL/latex test imports after refactor"
    This reverts commit 8dce4d9ac7be1d8aca5234e0df6cd74b4077fbd0.

--- ark/hitl/__init__.py
+++ ark/hitl/__init__.py
@@ -1,6 +1,5 @@
 from .utils import (
     _normalise_needs_human,
-    _coerce_hitl_options,
     _append_hitl_history,
     _update_hitl_decisions,
     _NONBLOCKING_URGENCIES,
--- ark/pipeline.py
+++ ark/pipeline.py
@@ -20,7 +20,6 @@ from ark.config import defaults
 
 from ark.hitl import (
     _normalise_needs_human,
-    _coerce_hitl_options,
     _append_hitl_history,
     _update_hitl_decisions,
     _NONBLOCKING_URGENCIES,
